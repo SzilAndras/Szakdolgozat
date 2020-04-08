c309c6ec14e962083f63ffbbe8345c93cccd9c66
szilagyiandras <szilagyi.andras@autsoft.hu>
2020-04-08 22:30:10 +0200

    Init reservation service.

---
+++ reservation-service/src/test/java/hu/bme/aut/reservationservice/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.reservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
